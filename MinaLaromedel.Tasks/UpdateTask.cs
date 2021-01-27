1952a66969922bf66cb94e17ab7d8393afaa4beb
Mikael Dúi Bolinder <mikael@bolinder.uk>
2021-01-27 02:04:36 +0100

    Recreated the tasks project.

---
+++ MinaLaromedel.Tasks/UpdateTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace MinaLaromedel.Tasks
+{
+    public sealed class UpdateTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // your app migration/update code here
+        }
+    }
+}
